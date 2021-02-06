83a8070e10e747414723d63cf4dbbda877b65368
Mauro Rezende <maurorezende2012@gmail.com>
2021-02-06 16:32:12 -0300

    Referenciando os posts do usuário

--- src/main/java/com/educandoweb/workshopmongo/domain/User.java
+++ src/main/java/com/educandoweb/workshopmongo/domain/User.java
@@ -1,21 +1,35 @@
 package com.educandoweb.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-    @Id
+
+	@Id
 	private String id;
 	private String name;
 	private String email;
 
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
 	public User() {
 
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	public User(String id, String name, String email) {
@@ -24,7 +38,6 @@ public class User implements Serializable {
 		this.name = name;
 		this.email = email;
 	}
-	
 
 	public String getId() {
 		return id;
